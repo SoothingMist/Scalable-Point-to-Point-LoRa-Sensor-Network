412842b79362513ac6a0080befb0133c9e1a4a38
Peter Raeth <peter_raeth@juno.com>
2025-03-04 07:41:09 -0500

    Add files via upload

---
+++ Documentation+Software/ErrorDetection/Duplex_Test_NodeA_CRC/Addresses.h
@@ -0,0 +1,9 @@
+
+// Node address and destination address.
+#ifndef Addresses
+#define Addresses
+
+uint8_t localAddress = 0xBB;     // address of this device
+uint8_t destination = 0xFF;      // destination to send to
+
+#endif
