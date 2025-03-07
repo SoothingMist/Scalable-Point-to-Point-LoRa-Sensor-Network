cde43282e29ce5400a9ba929b70a649d84a6750f
Peter Raeth <peter_raeth@juno.com>
2025-03-07 14:36:21 -0500

    Accesses common programmable hub

--- Documentation+Software/Programmable_USB_HUB_PingPong/ArduinoUno/ArduinoUno.ino
+++ Documentation+Software/Programmable_USB_HUB_PingPong/ArduinoUno/ArduinoUno.ino
@@ -1,13 +1,9 @@
 
 // Constants and variables used by various subroutines
-#define HANDSHAKE "H"
-uint8_t MESSAGE[256]; // LoRa max message length
-
-// Forward function declarations
-void Connect();
-void ForwardMessage();
-void ReceiveMessage();
-void Wait(unsigned long milliseconds);
+// Constants and variables regarding messages.
+const uint8_t HANDSHAKE = (uint8_t)'H';
+#define MAX_MESSAGE_LENGTH 256
+uint8_t MESSAGE[MAX_MESSAGE_LENGTH];
 
 void setup()
 {
@@ -26,7 +22,7 @@ void loop()
 
   // Assuming a one-byte message for this application
   MESSAGE[1]++;
-  ForwardMessage((String)(char)MESSAGE[1]);
+  ForwardMessage();
 }
 
 // ============= Function Definitions =================
@@ -37,12 +33,14 @@ void loop()
 void Connect()
 {
   // Send handshake
-  ForwardMessage(HANDSHAKE);
+  MESSAGE[0] = 02;
+  MESSAGE[1] = HANDSHAKE;
+  ForwardMessage();
 
   // Receive handshake
   MESSAGE[0] = 00;
   MESSAGE[1] = 00;
-  while((MESSAGE[0] != 01) && (MESSAGE[1] != (uint8_t)HANDSHAKE[0]))
+  while((MESSAGE[0] != 02) || (MESSAGE[1] != HANDSHAKE))
   {
     Wait(100);
     ReceiveMessage();
@@ -51,25 +49,33 @@ void Connect()
 
 void ReceiveMessage()
 {
-  while(Serial.available() <= 0) Wait(100); // wait for message
+  while( ! Serial.available()) Wait(100); // wait for message
   MESSAGE[0] = (uint8_t)Serial.read(); // get message size
-  for(uint8_t i = 01; i <= MESSAGE[0]; i++)
+  for(uint8_t i = 01; i < MESSAGE[0]; i++)
   {
     while( ! Serial.available()) Wait(100); // wait for message
     MESSAGE[i] = (uint8_t)Serial.read(); // read message
   }
 }
 
-void ForwardMessage(String thisString)
+void ForwardMessage()
 {
-  const uint8_t* message = (const uint8_t*)thisString.c_str();
-  uint8_t messageLength = thisString.length();
-  Serial.write(messageLength);
-  for(uint8_t i = 00; i < messageLength; i++) Serial.write(message[i]);
+  for(uint8_t i = 00; i < MESSAGE[0]; i++) Serial.write(MESSAGE[i]);
 }
 
-void Wait(unsigned long milliseconds) // delay() is blocking so we do not use that
+void ForwardMessage(String text)
 {
-  unsigned long beginTime = millis();
-  while ((millis() - beginTime) < milliseconds);
+  MESSAGE[0] = (uint8_t)text.length() + 1;
+  sprintf((char*)(MESSAGE + 1), "%s", text.c_str());
+  ForwardMessage();
+}
+
+// Wait for a specific number of milliseconds.
+// delay() is blocking so we do not use that.
+// This approach does not use hardware-specific timers.
+void Wait(long milliseconds)
+{
+  long beginTime = millis();
+  uint8_t doSomething = 00;
+  while ((millis() - beginTime) <= milliseconds) doSomething++;
 }
