9cf02cacaf9b89e270c9f064175f5bd1fa1b5ef1
Peter Raeth <peter_raeth@juno.com>
2025-04-04 15:43:35 -0400

    Delete Documentation+Software/Duplex_Test_NodeA directory

--- Documentation+Software/Duplex_Test_NodeA/Addresses.h
+++
@@ -1,9 +0,0 @@
-
-// Node address and destination address.
-#ifndef Addresses
-#define Addresses
-
-byte localAddress = 0xBB;     // address of this device
-byte destination = 0xFF;      // destination to send to
-
-#endif
