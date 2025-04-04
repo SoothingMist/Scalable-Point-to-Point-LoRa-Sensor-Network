82eaa2119bcc0928856c992851f3cb5239e5c8a7
Peter Raeth <peter_raeth@juno.com>
2025-04-04 15:44:30 -0400

    Add files via upload

---
+++ Documentation+Software/Duplex_Test/Duplex_Test_NodeA/Addresses.h
@@ -0,0 +1,9 @@
+
+// Node address and destination address.
+#ifndef Addresses
+#define Addresses
+
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xFF;      // destination to send to
+
+#endif
