e27195d765979b4e5e621a567b2f4d38fcb79be5
Peter Raeth <peter_raeth@juno.com>
2025-06-18 10:59:34 -0400

    Add files via upload

--- Documentation+Software/SimpleSensor/SimpleSensor.ino
+++ Documentation+Software/SimpleSensor/SimpleSensor.ino
@@ -1,16 +1,18 @@
 
-// This particular sensor node sends the voltage read
+// This particular sensor node reads the voltage
 // at a given input pin.
 
 // Adds Arduino's language capabilities.
 // https://stackoverflow.com/questions/10612385/strings-in-c-class-file-for-arduino-not-compiling
 #include <Arduino.h>
 
-// See setup() for references.
 // The resolution is the number of ADC levels.
 // https://electronics.stackexchange.com/questions/406906/how-to-obtain-input-voltage-from-adc-value
 #define analogResolution 4096.0f
-#define voltageReference 5.0f
+
+// The voltage reference is 3.3v.
+// https://docs.arduino.cc/language-reference/en/functions/analog-io/analogReference
+#define voltageReference 3.3
 
 // Identify the input pin
 #define inputPin A1
@@ -27,7 +29,7 @@ void setup()
 
   // Configure analog digital conversion (ADC).
   // MKR WAN 1310 is a SAMD board.
-  // Default reference is 5v.
+  // Default reference is given by AR_DEFAULT.
   // https://docs.arduino.cc/language-reference/en/functions/analog-io/analogReference
   // Capable of 12-bit ADC resolution.
   // This yields ADC output of 0 .. 4095.
