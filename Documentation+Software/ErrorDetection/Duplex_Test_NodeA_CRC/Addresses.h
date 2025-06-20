9589eeb2c9eeb50bc6ea00f53650346f94180a82
Peter Raeth <peter_raeth@juno.com>
2025-06-20 13:41:36 -0400

    Delete Documentation+Software/ErrorDetection/Duplex_Test_NodeA_CRC directory

--- Documentation+Software/ErrorDetection/Duplex_Test_NodeA_CRC/Addresses.h
+++
@@ -1,9 +0,0 @@
-
-// Node address and destination address.
-#ifndef Addresses
-#define Addresses
-
-uint8_t localAddress = 0xBB;     // address of this device
-uint8_t destination = 0xFF;      // destination to send to
-
-#endif
