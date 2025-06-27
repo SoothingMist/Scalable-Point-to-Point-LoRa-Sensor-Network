626723c3173c579f1abac1487981d6370f4d0f91
Peter Raeth <peter_raeth@juno.com>
2025-06-27 14:14:29 -0400

    Add files via upload

--- Documentation+Software/Programmable_USB_HUB/ExpandedCameraAccess/Uno/Uno.ino
+++ Documentation+Software/Programmable_USB_HUB/ExpandedCameraAccess/Uno/Uno.ino
@@ -1,53 +1,51 @@
 
+// Makes use of this debugging technique:
+// https://github.com/SoothingMist/Arduino-as-USB-to-Serial-Adaptor.
+//#define DEBUG
+
+// Adds ability to pack and forward camera data.
+// Initializes Pixy camera.
+#include "CameraData.h"
+
 // Constants and variables used by various subroutines
 // Constants and variables regarding messages.
 const uint8_t HANDSHAKE = (uint8_t)'H';
-#define MAX_MESSAGE_LENGTH 256
-uint8_t MESSAGE[MAX_MESSAGE_LENGTH];
 uint8_t SIZE_OUTPUT_BUFFER = 0;
 
 void setup()
 {
   // Initialize Arduino
-  Serial.begin(9600);
-  while(!Serial) Wait(100); // make sure Serial is ready
-
+  Serial.begin(9600); while(!Serial) Wait(100);
+  #ifdef DEBUG
+    Serial1.begin(9600); while(! Serial1) Wait(100);
+  #endif
+  
   // Get size of this device's serial output buffer
   // https://www.arduino.cc/reference/tr/language/functions/communication/serial/availableforwrite
   SIZE_OUTPUT_BUFFER = Serial.availableForWrite();
 
   // Wait for connection with external device
-  TransceiverConnect();
-
-  // Make sure the input buffer is empty.
-  while(Serial.available()) Serial.read();
-
-  // Send request for entire image
-  MESSAGE[0] = 2;
-  MESSAGE[1] = HANDSHAKE;
-  ForwardMessage();
+  DataConnect();
+  #ifdef DEBUG
+    Serial1.print("Respondent ready\n"); Serial1.flush();
+  #endif
+  
+  // Initialize the camera.
+  // Could send an error message here.
+  if( ! InitializeCamera()) exit(1);
 }
 
 void loop()
 {
-  static bool continueLoop = true;
-  while(continueLoop) // stop upon "Done" message
-  {
-    // Send "ready for next segment".
-    MESSAGE[0] = 2;
-    MESSAGE[1] = HANDSHAKE;
-    ForwardMessage();
-  
-    // Wait for a message
-    ReceiveMessage();
+  // Wait for picture to be requested.
+  #ifdef DEBUG
+    Serial1.print("Waiting to receive message\n"); Serial1.flush();
+  #endif
+  ReceiveMessage();
 
-    // Do whatever with this image segment.
-    // Ultimately, transmit via LoRa.
-    if(MESSAGE[0] == 5) // look for "Done" message
-    {
-      continueLoop = false;
-    } 
-  }
+  // Assuming for now that the request is for
+  // an entire image to be sent.
+  GatherCameraData();
 }
 
 // ============= Function Definitions =================
@@ -55,21 +53,21 @@ void loop()
 // Handshake with the connected device.
 // Reject any input but the handshake.
 // Both devices must use the same handshake.
-void TransceiverConnect()
+void DataConnect()
 {
-  // Send handshake
-  MESSAGE[0] = 2;
-  MESSAGE[1] = HANDSHAKE;
-  ForwardMessage();
-
   // Receive handshake
-  MESSAGE[0] = 0;
-  MESSAGE[1] = 0;
-  while((MESSAGE[0] != 2) || (MESSAGE[1] != HANDSHAKE))
+  MESSAGE[0] = 00;
+  MESSAGE[1] = 00;
+  while((MESSAGE[0] != 02) || (MESSAGE[1] != HANDSHAKE))
   {
     Wait(100);
     ReceiveMessage();
   }
+  
+  // Send handshake
+  MESSAGE[0] = 02;
+  MESSAGE[1] = HANDSHAKE;
+  ForwardMessage();
 }
 
 void ReceiveMessage()
@@ -89,6 +87,9 @@ void ReceiveMessage()
     MESSAGE[byteCount] = (uint8_t)inputByte;
     byteCount++;
   }
+  #ifdef DEBUG
+    Serial1.print("Read message of length "); Serial1.println(MESSAGE[0]); Serial1.flush();
+  #endif
 }
 
 void ForwardMessage()
@@ -96,7 +97,10 @@ void ForwardMessage()
   // Notice that MESSAGE[0] is the total length of the message,
   // not the number of following bytes. This means the number of
   // following bytes is one less than MESSAGE[0].
-  
+
+  #ifdef DEBUG
+    Serial1.print("Message Length: "); Serial1.println(MESSAGE[0]); Serial1.flush();
+  #endif
   Serial.write(MESSAGE[0]); // write one message byte
   Serial.flush();
   uint8_t numBlocks = (uint8_t)((MESSAGE[0] - 1) / SIZE_OUTPUT_BUFFER);
